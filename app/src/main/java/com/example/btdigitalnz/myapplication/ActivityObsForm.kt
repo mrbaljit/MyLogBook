4fd94acdfd3fb90992597d8ad6916c59b96ccb7e
Baljit <mrbaljit@gmail.com>
2018-07-30 22:32:06 +1200

    test

--- app/src/main/java/com/example/btdigitalnz/myapplication/ActivityObsForm.kt
+++ app/src/main/java/com/example/btdigitalnz/myapplication/ActivityObsForm.kt
@@ -4,6 +4,7 @@ package com.example.btdigitalnz.myapplication
 import android.app.DatePickerDialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.View
 import kotlinx.android.synthetic.main.activity_obs_form.*
 import java.text.SimpleDateFormat
@@ -21,6 +22,7 @@ class ActivityObsForm : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_obs_form)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
 
         val spinner = findViewById<View>(R.id.spinner) as Spinner
 // Create an ArrayAdapter using the string array and a default spinner layout
@@ -32,14 +34,14 @@ class ActivityObsForm : AppCompatActivity() {
         spinner.adapter = adapter
 
    btn_show_date.setOnClickListener {
-       var formate = SimpleDateFormat("dd-MMM-YYYY", Locale.ENGLISH)
+       var dtFormat = SimpleDateFormat("dd-MMM-YYYY", Locale.ENGLISH)
        var now = Calendar.getInstance()
             val datePicker = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
                 //val selectedDate = Calendar.getInstance()
                 now.set(Calendar.YEAR,year)
                 now.set(Calendar.MONTH,month)
                 now.set(Calendar.DAY_OF_MONTH,dayOfMonth)
-                val date = formate.format(now.time)
+                val date = dtFormat.format(now.time)
                 btn_show_date.text = date
             },
                     now.get(Calendar.YEAR),now.get(Calendar.MONTH),now.get(Calendar.DAY_OF_MONTH))
@@ -48,11 +50,15 @@ class ActivityObsForm : AppCompatActivity() {
    }
 
 
+
+
     }
 
-
-
-
+    // create an action bar button
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.obsmenu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
 
 }
